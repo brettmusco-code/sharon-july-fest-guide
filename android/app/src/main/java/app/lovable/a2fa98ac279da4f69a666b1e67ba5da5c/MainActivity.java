ce77a74895d2d934cf06324258c4071143c2a022
Brett Musco <bmusco@cmtelematics.com>
2026-04-19 21:07:32 -0400

    Add native iOS/Android (Capacitor); load bundled app instead of Lovable preview

---
+++ android/app/src/main/java/app/lovable/a2fa98ac279da4f69a666b1e67ba5da5c/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.a2fa98ac279da4f69a666b1e67ba5da5c;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.a2fa98ac279da4f69a666b1e67ba5da5c',
   appName: 'sma-july4th',
   webDir: 'dist',
-  server: {
-    url: 'https://2fa98ac2-79da-4f69-a666-b1e67ba5da5c.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
 };
 
 export default config;
